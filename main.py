044487bbd7ef670a2435556ee80bbee406ac1afd
sachin1199 <sachinsingh94070@gmail.com>
2025-11-01 20:54:49 +0530

    fixed health route

--- main.py
+++ main.py
@@ -29,5 +29,5 @@ def stop():
 
 if __name__ == "__main__":
     import os
-    port = int(os.environ.get("PORT", 5000))  # Render dynamically assigns a port
+    port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
